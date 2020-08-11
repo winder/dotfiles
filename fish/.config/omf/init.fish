30f50a045a579728e6bfcdf5cd62f7943976a969
Will Winder <wwinder.unh@gmail.com>
2020-08-11 11:10:06 -0400

    More fish variables.

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -4,3 +4,7 @@ alias ag="rg"
 
 alias gosrc="cd ~/go/src/github.com/algorand/go-algorand/"
 alias src="cd ~/algorand/"
+
+set -x GOROOT /opt/go/go
+set -x GOPATH ~/go
+set -x PATH $PATH $GOROOT/bin $GOPATH/bin
