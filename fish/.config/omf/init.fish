4af8cfcb857b9cd2416644c8047ad9ceb26f93ac
Will Winder <wwinder.unh@gmail.com>
2020-11-24 10:20:49 -0500

    Random changes that have been accumulating.

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -8,7 +8,29 @@ if status --is-interactive
   abbr src 'cd ~/algorand/'
 end
 
+set -g theme_date_timezone America/New_York
+
+set -gx PATH $PATH /opt/java/jdk-13+33/bin
+set -gx PATH $PATH ~/scripts
+
 set -gx ALGORAND_DATA ~/.algorand
 set -gx GOROOT /opt/go/go
 set -gx GOPATH ~/go
 set -gx PATH $PATH $GOROOT/bin $GOPATH/bin
+
+# java
+
+# graalvm
+#set -gx GRAALVM_HOME /opt/java/graal/graalvm-ce-java11-20.2.0/
+#set -gx JAVA_HOME $GRAALVM_HOME
+
+
+#set -gx JAVA_HOME /opt/java/jdk1.8.0_202
+#set -gx JAVA_HOME /opt/java/openjdk-11.0.2
+#set -gx JAVA_HOME /opt/java/jdk-11.0.8+10
+#set -gx JAVA_HOME /opt/java/jdk-12.0.2+10
+#set -gx JAVA_HOME /opt/java/jdk-13+33
+#set -gx JAVA_HOME /opt/java/adopt-openj9-14.0.2
+set -gx JAVA_HOME /opt/java/jdk-15+36
+set -gx PATH $JAVA_HOME/bin $PATH
+
