36ed96a6d12840e48f42c0c52a96b76005d6ba82
Will Winder <wwinder.unh@gmail.com>
2024-11-02 19:39:14 -0400

    misc

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:yubico/stable
 sudo apt update
 
 # install packages
-sudo apt-get install \
+sudo apt-get install -y \
   acpi \
   autoconf \
   ca-certificates \
@@ -48,6 +48,7 @@ sudo apt-get install \
   pasystray \
   pavucontrol \
   vlc \
+  openjdk-17-jre \
   google-chrome-stable
 
   #libappindicator3-1 \
@@ -55,7 +56,7 @@ sudo apt-get install \
   #i3xrocks-weather \
 
 # yubikey // rustica agent // signed commits // rust
-sudo apt install \
+sudo apt install -y \
   libpcsclite-dev \
   libudev-dev \
   libpam-yubico \
@@ -108,30 +109,31 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker || true # probably setup by apt
 sudo usermod -aG docker $USER
 
 # fix for "peek": https://github.com/phw/peek/issues/677#issuecomment-759050507
-gsettings set org.gnome.gnome-flashback screencast false
+gsettings set org.gnome.gnome-flashback screencast false || true # in case gnome isn't used
 
 # install golang
 # install language server for neovim
 #go install golang.org/x/tools/gopls@latest
 
 # install vscode - https://code.visualstudio.com/docs/setup/linux
-sudo apt-get install wget gpg
+sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 sudo apt update
-sudo apt install code # or code-insiders
+sudo apt install -y code # or code-insiders
 
 # install platformio
 wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O get-platformio.py
+sudo apt install -y python3-venv
 python3 get-platformio.py
 
 # platformio udev rules
